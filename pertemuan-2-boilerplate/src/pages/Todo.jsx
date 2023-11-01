1a919439c2c0341d476bb9eebd301b4c7c51ab81
Ahmad Fauzan <57349734+afzn9x@users.noreply.github.com>
2023-11-01 09:29:26 +0700

    update

---
+++ pertemuan-2-boilerplate/src/pages/Todo.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+const Todo = () => {
+    main()
+    return (
+        <div>
+            <h2>Check in your console</h2>
+        </div>
+    );
+}
+
+// Jangan mengubah apapun pada function main
+const main = () => {
+    console.log("# Get All Users");
+    all()
+
+    console.log("# Add New User: Sabiq");
+    const newUser = {
+        name: "Sabiq",
+        age: 20,
+        major: "Informatics"
+    };
+    store(newUser)
+
+    console.log("# Edit User: Isfa")
+    const editedUser = {
+        name: "Isfhani Ghiyath",
+        age: 23,
+        major: "English",
+    }
+    update(1, editedUser)
+
+    console.log("# Delete User: Nurul");
+    destroy(2)
+}
+
+
+// TODO 1 - Buatlah array of object users (5 users)
+// Objek memiliki property: name, age, major
+// Gunakan const untuk membuat variabel
+const users = []
+
+// TODO 2 - Membuat function yang menampilkan semua data users
+// Gunakan for/for-of
+const all = () => {
+    
+}
+
+// TODO 3 - Membuat function untuk menambahkan data users
+// Gunakan metode push
+const store = (user) => {
+    
+}
+
+// TODO 4 - Membuat function untuk mengupdate data users
+// Ganti data users berdasarkan index pada array
+const update = (index, user) => {
+    
+}
+
+// TODO 5 - Membuat function untuk menghapus data users berdasarkan index
+// Gunakan metode splice
+const destroy = (index) => {
+    
+}
+
+export default Todo;
